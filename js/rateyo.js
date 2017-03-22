05f3ea83f0d40936954365b64201c5164452e446
Ramy El Khayat <ramyelkhayat@Ramys-MacBook-Pro.local>
2017-03-22 13:44:58 -0400

    added comments on city page

---
+++ js/rateyo.js
@@ -0,0 +1,128 @@
+$(function () {
+ 
+  $("#education-rating").rateYo({
+    normalFill:"#FAEBD7",
+    starWidth : "20px",
+    multiColor: {
+ 
+      "startColor": "#b44545", //RED
+      "endColor"  : "#80ba52"//GREEN
+    },
+    fullStar: true
+  });
+  $("#crime-rating").rateYo({
+    normalFill:"#FAEBD7",
+    starWidth : "20px",
+    multiColor: {
+ 
+      "startColor":  "#80ba52", //GREEN
+      "endColor"  :   "#b44545"//RED
+    },
+    fullStar: true
+  });
+  $("#entertainment-rating").rateYo({
+    normalFill:"#FAEBD7",
+    starWidth : "20px",
+    multiColor: {
+ 
+      "startColor": "#b44545", //RED
+      "endColor"  : "#80ba52"//GREEN
+    },
+    fullStar: true
+  });
+  $("#housing-rating").rateYo({
+    normalFill:"#FAEBD7",
+    starWidth : "20px",
+    multiColor: {
+ 
+      "startColor": "#b44545", //RED
+      "endColor"  : "#80ba52"//GREEN
+    },
+    fullStar: true
+  });
+  $("#happiness-rating").rateYo({
+    normalFill:"#FAEBD7",
+    starWidth : "20px",
+    multiColor: {
+ 
+      "startColor": "#b44545", //RED
+      "endColor"  : "#80ba52"//GREEN
+    },
+    fullStar: true
+  });
+  $("#healthcare-rating").rateYo({
+    normalFill:"#FAEBD7",
+    starWidth : "20px",
+    multiColor: {
+ 
+      "startColor": "#b44545", //RED
+      "endColor"  : "#80ba52"//GREEN
+    },
+    fullStar: true
+  });
+ 
+});
+$(function () {
+ 
+  $("#education-rating-comment").rateYo({
+    normalFill:"#FAEBD7",
+    starWidth : "20px",
+    multiColor: {
+ 
+      "startColor": "#b44545", //RED
+      "endColor"  : "#80ba52"//GREEN
+    },
+    fullStar: true
+  });
+  $("#crime-rating-comment").rateYo({
+    normalFill:"#FAEBD7",
+    starWidth : "20px",
+    multiColor: {
+ 
+      "startColor":  "#80ba52", //GREEN
+      "endColor"  :   "#b44545"//RED
+    },
+    fullStar: true
+  });
+  $("#entertainment-rating-comment").rateYo({
+    normalFill:"#FAEBD7",
+    starWidth : "20px",
+    multiColor: {
+ 
+      "startColor": "#b44545", //RED
+      "endColor"  : "#80ba52"//GREEN
+    },
+    fullStar: true
+  });
+  $("#housing-rating-comment").rateYo({
+    normalFill:"#FAEBD7",
+    starWidth : "20px",
+    multiColor: {
+ 
+      "startColor": "#b44545", //RED
+      "endColor"  : "#80ba52"//GREEN
+    },
+    fullStar: true
+  });
+  $("#happiness-rating-comment").rateYo({
+    normalFill:"#FAEBD7",
+    starWidth : "20px",
+    multiColor: {
+ 
+      "startColor": "#b44545", //RED
+      "endColor"  : "#80ba52"//GREEN
+    },
+    fullStar: true
+  });
+  $("#healthcare-rating-comment").rateYo({
+    normalFill:"#FAEBD7",
+    starWidth : "20px",
+    multiColor: {
+ 
+      "startColor": "#b44545", //RED
+      "endColor"  : "#80ba52"//GREEN
+    },
+    fullStar: true
+  });
+ 
+});
